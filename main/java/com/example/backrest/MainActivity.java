81d97ef8f1e48e15143a0bde4ec185b3f0d4be3e
Spataru HoriaStefan <horia.spataru01@e-uvt.ro>
2025-07-23 00:12:42 +0300

    added dark mode

--- main/java/com/example/backrest/MainActivity.java
+++ main/java/com/example/backrest/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.backrest;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.graphics.Color;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity
 {
+    private ConstraintLayout mainLayout;
+    private Switch darkLightSwitch;
+    private TextView welcomeMessage;
+
     //main activity
     @Override protected void onCreate(Bundle savedInstanceState) //bundle object parameter contains the saved state of the activity
     {
@@ -21,6 +31,33 @@ public class MainActivity extends AppCompatActivity
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars()); //get margins of system bars
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom); //set margins of view
             return insets; //return margins of view
+        });
+
+        mainLayout = findViewById(R.id.main);
+        darkLightSwitch = findViewById(R.id.darklight);
+        welcomeMessage = findViewById(R.id.welcome_message);
+
+        darkLightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //the method is called when the switch state changes
+
+                if (isChecked)
+                {
+                    mainLayout.setBackgroundColor(Color.BLACK);
+                    welcomeMessage.setTextColor(Color.WHITE);
+                    darkLightSwitch.setText("Light");
+                    darkLightSwitch.setTextColor(Color.WHITE);
+                }
+                else
+                {
+                    mainLayout.setBackgroundColor(Color.WHITE);
+                    welcomeMessage.setTextColor(Color.DKGRAY);
+                    darkLightSwitch.setText("Dark");
+                    darkLightSwitch.setTextColor(Color.BLACK);
+                }
+            }
         });
     }
 }
